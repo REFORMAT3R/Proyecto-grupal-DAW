c46e55d4a474f845141174694f198c681b2b48a5
oguevara-code <oguevara@unsa.edu.pe>
2026-04-21 10:19:16 -0500

    Corrigiendo la corrección

--- Calculator.java
+++ Calculator.java
@@ -1,15 +1,9 @@
 class Calculator {
-int add(int a, int b){ return 0; } //Hace Mario
-int sub(int a, int b){ return 0; } //Hace Jazmin
-int mul(int numero1, int numero2){         //Hace Omar
-    return numero1 * numero2; 
-} 
-
-int div(int a, int b){ return 0; } //Hace Denny
-int mod(int a, int b){ return 0; } //Hace Jhovi
     int add(int a, int b){ return 0; } //Hace Mario
     int sub(int a, int b){ return 0; } //Hace Jazmin
-    int mul(int a, int b){ return 0; } //Hace Omar
+    int mul(int numero1, int numero2){ //Hace Omar
+        return numero1 * numero2; 
+    } 
     double div (double numero1, double numero2){ 
          if(numero2 == 0) {
             System.out.println("No se puede dividir entre 0");
