e5b85af2567c72cb61424f5d2173579a8dd13944
Shvy-bit <jcusiq@unsa.edu.pe>
2026-04-21 10:40:30 -0500

    Arreglo merge

--- Calculator.java
+++ Calculator.java
@@ -1,32 +1,26 @@
 class Calculator {
-int add(int a, int b){ return 0; }{
-    int suma = a+b;
-    System.out.println("El resultado de la adición es:")
-    return suma;
-}//Hace Mario
-int sub(int a, int b){ return 0; } //Hace Jazmin
-int mul(int numero1, int numero2){         //Hace Omar
-    return numero1 * numero2; 
-} 
-int mod(int a, int b){ return 0; } //Hace Jhovi
-<<<<<<< HEAD
-
+    int add(int a, int b){
+        int suma = a + b;
+        System.out.println("El resultado de la adición es:");
+        return suma;
+    }//Hace Mario
+    int sub(int a, int b){ return 0; } //Hace Jazmin
+    int mul(int numero1, int numero2){         //Hace Omar
+        return numero1 * numero2;
+    }
+    int mod(int a, int b){ return 0; } //Hace Jhovi
     double sub(double a, double b){
         return a-b ;
     } //Jazmin
-
     double div (double numero1, double numero2){ 
-         if(numero2 == 0) {
+        if(numero2 == 0) {
             System.out.println("No se puede dividir entre 0");
             return 0;
-         }
-         else {
+        }
+        else {
             double resultado = numero1 / numero2;
             resultado = Math.round(resultado * 100.0) / 100.0;
             return resultado;
-         }
+        }
     } //Hace Denny
-
-=======
->>>>>>> Metodoadd
 }
