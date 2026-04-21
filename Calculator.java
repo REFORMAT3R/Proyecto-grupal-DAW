ec999c67de610c95bfe7ea7632180a03f137fbc3
Haku021 <jgonzalesra@unsa.edu.pe>
2026-04-21 09:51:27 -0500

    metodo sub

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,10 @@
 class Calculator {
 int add(int a, int b){ return 0; } //Hace Mario
-int sub(int a, int b){ return 0; } //Hace Jazmin
+public int sub(int a, int b){
+    int sub = a-b;
+    return sub;
+} //Jazmin
+
 int mul(int a, int b){ return 0; } //Hace Omar
 int div(int a, int b){ return 0; } //Hace Denny
 int mod(int a, int b){ return 0; } //Hace Jhovi
