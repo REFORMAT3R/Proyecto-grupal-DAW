ad428442807e6c66c4ed64b9f4fd2fa190df5873
REFORMAT3R <dmamanihuayh@unsa.edu.pe>
2026-04-21 09:36:59 -0500

    Subiendo plantilla

---
+++ Calculator.java
@@ -0,0 +1,7 @@
+class Calculator {
+int add(int a, int b){ return 0; } //Hace Mario
+int sub(int a, int b){ return 0; } //Hace Jazmin
+int mul(int a, int b){ return 0; } //Hace Omar
+int div(int a, int b){ return 0; } //Hace Denny
+int mod(int a, int b){ return 0; } //Hace Jhovi
+}
