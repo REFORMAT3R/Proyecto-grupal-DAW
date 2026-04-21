2181cdc17c96cde62aa52e56f20995e98ef22386
Shvy-bit <jcusiq@unsa.edu.pe>
2026-04-21 09:55:36 -0500

    Metodo mod

--- Calculator.java
+++ Calculator.java
@@ -3,5 +3,13 @@ int add(int a, int b){ return 0; } //Hace Mario
 int sub(int a, int b){ return 0; } //Hace Jazmin
 int mul(int a, int b){ return 0; } //Hace Omar
 int div(int a, int b){ return 0; } //Hace Denny
-int mod(int a, int b){ return 0; } //Hace Jhovi
+public int mod(int a, int b){
+    int mod;
+    if(b == 0) {
+        System.out.println("Nose puede dividir por 0");
+        mod = 0;
+    }
+    else mod = a % b;
+    return mod;
+}
 }
