8d2ae60d509515dac41ab9c535b3aa1c2953acc0
Mario Pandia <mchoquecota@unsa.edu.pe>
2026-04-21 15:00:41 -0500

    aeamano

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,9 @@
 class Calculator {
-int add(int a, int b){ return 0; } //Hace Mario
+int add(int a, int b){ return 0; }{
+    int suma = a+b;
+    System.out.println("El resultado de la adición es:")
+    return suma;
+}//Hace Mario
 int sub(int a, int b){ return 0; } //Hace Jazmin
 int mul(int a, int b){ return 0; } //Hace Omar
 int div(int a, int b){ return 0; } //Hace Denny
